98eb2ca95353884cadc96071b795cd39028eb961
BENZ <naruepanart1201@gmail.com>
2019-03-22 15:22:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 // Config API
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');  
+/* const posts = require('./routes/api/posts');   */
 
 // Fetch genkey
 const keyMongodb = 'mongodb+srv://benzmasters:WLOJ3L4t63MTu3mN@cluster0-pj6ax.mongodb.net/main?retryWrites=true';
@@ -38,7 +38,7 @@ require('./config/passport.js')(passport);
 // Config Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
-app.use('/api/posts', posts);  
+/* app.use('/api/posts', posts);   */
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
