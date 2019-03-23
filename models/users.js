16ce71d74b08ae0d807ea238ae0e7b2345fb7d58
BENZ <naruepanart1201@gmail.com>
2019-03-23 11:50:41 +0700

    Update users.js

--- models/users.js
+++ models/users.js
@@ -4,6 +4,9 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  stufirstname: { type: String, required: true },
+  stulastname: { type: String, required: true },
+  
   update: { type: Date, default: Date.now }
 }, { collection: 'users' }
 );
