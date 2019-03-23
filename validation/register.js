654f02ef9bea8c5cc49dc5e0f79c647b09d73a4f
BENZ <naruepanart1201@gmail.com>
2019-03-23 11:58:42 +0700

    Update register.js

--- validation/register.js
+++ validation/register.js
@@ -6,7 +6,9 @@ module.exports = function validateRegisterInput(data) {
 
   data.username = !isEmpty(data.username) ? data.username : '';
   data.password = !isEmpty(data.password) ? data.password : '';
-/*   data.password2 = !isEmpty(data.password2) ? data.password2 : ''; */
+  data.stufirstname = !isEmpty(data.stufirstname) ? data.stufirstname : '';
+  data.stulastname = !isEmpty(data.stulastname) ? data.stulastname : '';
+  /*   data.password2 = !isEmpty(data.password2) ? data.password2 : ''; */
 
   if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
     errors.username = 'username must be between 2 and 30 characters';
@@ -23,14 +25,20 @@ module.exports = function validateRegisterInput(data) {
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = 'Password must be at least 6 characters';
   }
+  if (Validator.isEmpty(data.stufirstname)) {
+    errors.stufirstname = 'Firstname field is required';
+  }
+  if (Validator.isEmpty(data.stulastname)) {
+    errors.stulastname = 'Lastname field is required';
+  }
 
-/*   if (Validator.isEmpty(data.password2)) {
-    errors.password2 = 'Confirm Password field is required';
-  } else {
-    if (!Validator.equals(data.password, data.password2)) {
-      errors.password2 = 'Passwords must match';
-    }
-  } */
+  /*   if (Validator.isEmpty(data.password2)) {
+      errors.password2 = 'Confirm Password field is required';
+    } else {
+      if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = 'Passwords must match';
+      }
+    } */
 
   return {
     errors,
