fb6a2263decfb2e7a1fbb7d3e685b74ee56f47f6
BENZ <naruepanart1201@gmail.com>
2019-03-25 21:16:20 +0700

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: { type: String, required: true },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: { type: String, required: true },
+      date: { type: Date, default: Date.now }
+    }
+  ],
+  date: { type: Date, default: Date.now }
+}, { collection: 'post' }
+);
+
+module.exports = mongoose.model('post', PostSchema);
