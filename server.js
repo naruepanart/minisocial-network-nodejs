da9d80b111b0f111da0bb47e5b80237e8749f697
BENZ <naruepanart1201@gmail.com>
2019-03-27 18:40:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-/* const cors = require('cors'); */
+const cors = require('cors');
 
 const app = express();
 
-/* app.use(cors()); */
+app.use('*', cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.send('hello world')
