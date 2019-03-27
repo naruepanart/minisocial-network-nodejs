5d8c910ef5d9d4e36a5954581865532600969442
BENZ <naruepanart1201@gmail.com>
2019-03-27 17:31:03 +0700

    update cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
