63c749cf5a9dcb4a5b27d296ae5bab96f8da9b18
BENZ <naruepanart1201@gmail.com>
2019-03-27 20:54:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const cors = require('cors');
+
 
 const app = express();
 
-app.use(cors());
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header(
+    'Access-Control-Allow-Methods',
+    'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+  )
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Option')
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  next()
+})
 
 app.get('/', function (req, res) {
   res.send('hello world')
@@ -32,7 +37,7 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // Fetch genkey
-const keyMongodb = 'mongodb+srv://benzmasters:2tH4i1Pyq2qCOrId@cluster0-pj6ax.mongodb.net/test?retryWrites=true'; 
+const keyMongodb = 'mongodb+srv://benzmasters:2tH4i1Pyq2qCOrId@cluster0-pj6ax.mongodb.net/test?retryWrites=true';
 
 var connectMongodb = process.env.MONGODB_URI || keyMongodb;
 
